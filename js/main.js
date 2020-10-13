f817236b3c515c3625a8aac1fdc0d32fa492527d
Santos Ivan Alva Chavez <ivanals8998@gmail.com>
2020-10-13 17:40:25 +0200

    js-pwdgen-wannabe

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//1. Chiedi il nome
+var nome = prompt ('Quale e il tuo nome?');
+
+//2. Chiedi il cognome
+var cognome = prompt ('Quale e il tuo cognome?');
+
+//3. Chiedi il colore preferito
+var favColore = prompt('Quale e il tuo colore preferito?');
+
+//4.Output
+document.getElementById('password').innerHTML = nome + cognome + favColore + '19';
